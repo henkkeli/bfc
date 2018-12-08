9489206966124b0c429f7cc11785a913e3188dbd
Henri Myllyoja <henri.myllyoja@gmail.com>
2018-12-08 19:31:05 +0200

    document set_arch

--- src/arch.h
+++ src/arch.h
@@ -3,6 +3,15 @@
 
 #include "common.h"
 
+/**
+ * @brief sets target architecture and some architecture specific options
+ *
+ * defaults to native. in case of native arch, also sets native flag.
+ *
+ * @param opts options structure
+ *
+ * @return 0 on success, -1 if architecture not supported
+ */
 int set_arch(struct options *opts);
 
 #endif /* ARCH_H */
